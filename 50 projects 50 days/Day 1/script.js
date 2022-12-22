6fb93e9567e5eab4d01031c67bf32da5719a0b0b
SampadSikder <sampadsikder84@gmail.com>
2022-12-22 13:11:33 +0600

    50 projects 50 days

---
+++ 50 projects 50 days/Day 1/script.js
@@ -0,0 +1,17 @@
+'using strict mode'
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    })
+});
+
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    })
+}

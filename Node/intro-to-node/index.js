1ebda420c18cbe15ed1804b3a7dba4a0b38dcc0c
SampadSikder <sampadsikder84@gmail.com>
2023-01-08 00:02:25 +0600

    Node try

---
+++ Node/intro-to-node/index.js
@@ -0,0 +1,9 @@
+
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperHero = superheroes.random();
+var mySuperVillains = supervillains.random();
+
+console.log(mySuperHero);
+console.log(mySuperVillains)

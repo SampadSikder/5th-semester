4009f842cdd62578178f21ac4f0142ee1bcc0407
SampadSikder <bsse1219@iit.du.ac.bd>
2023-01-15 17:18:34 +0600

    html

---
+++ Web tech/Js/index.js
@@ -0,0 +1,18 @@
+// //alert('hi')
+
+
+// var x = 10;
+// var y = 20;
+
+// console.log(x + y)
+
+// if (y == 10) {
+//     console.log("nc");
+// }
+
+// let a = 20;
+// let b = "20";
+
+// if (a == b) {
+//     console.log("matched")
+// }

394aad0191ca6a9d4afde4d40e9352b0077afbc4
SampadSikder <bsse1219@iit.du.ac.bd>
2023-02-13 09:54:03 +0600

    Added php

---
+++ Web tech/PHP/try/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+    <form action="welcome.php" method="get">
+        National ID: <input type="text" name="name"><br>
+        Date of birth: <br>
+        <input type="date" name="dob"><br>
+        <input type="submit">
+    </form>
+
+</body>
+
+</html>

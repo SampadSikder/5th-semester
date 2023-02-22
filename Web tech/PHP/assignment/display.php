942b9458d6a4f3cbd58764ecda3ec87771e0cba1
SampadSikder <sampadsikder84@gmail.com>
2023-02-22 21:02:08 +0600

    php complete

--- Web tech/PHP/assignment/display.php
+++ Web tech/PHP/assignment/display.php
@@ -5,47 +5,54 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <style>
+        body {
+            font-style: bold;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 20px;
+        }
+
         .upldimg {
-            display: inline;
+            float: right;
+            padding-right: 50px;
         }
     </style>
 </head>
 
 <body>
-    <?php
-    $fileName = $_FILES['photo']['name'];
+    <?php foreach ($_POST as $val) {
+        if (empty($val))
+            echo "<script>alert('You have not filled up all the inputs')</script>";
+    }
     ?>
-    <div class="row">
-        <div class="col-lg-5">
-            Name:
-            <?php
-            echo $_POST["fName"] . " " . $_POST["lName"];
-            ?>
-        </div>
-        <div class="col-lg-5">
-            <div class="upldimg">
-                <?php
-                echo "<image src= $fileName>";
-                ?>
-            </div>
-        </div>
+    <?php
+    $photoName = $_FILES['photo']['name'];
+    ?>
+
+
+    Name:
+    <?php
+    echo $_POST["fName"] . " " . $_POST["lName"];
+    ?><br>
+
+
+    <div class="upldimg">
+        <?php
+        $im = "<img src=$photoName width=\"300px;\" style=\"border-radius: 200px\">";
+        echo $im;
+        ?>
     </div>
-    <div class="row">
-        <div class="col-lg-5">
-            Email:
-            <?php
-            echo $_POST["email"];
-            ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-5">
-            Address:
-            <?php
-            echo $_POST["addr"];
-            ?>
-        </div>
-    </div>
+
+    Email:
+    <?php
+    echo $_POST["email"];
+    ?><br>
+
+    Address:
+    <?php
+    echo $_POST["addr"];
+    ?><br>
+
+
 
 
 

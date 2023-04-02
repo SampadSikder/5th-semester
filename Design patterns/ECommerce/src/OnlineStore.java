eb799f4552aba23580dfc7c8965d645bee438176
SampadSikder <sampadsikder84@gmail.com>
2023-04-02 23:52:37 +0600

    final method

--- Design patterns/ECommerce/src/OnlineStore.java
+++ Design patterns/ECommerce/src/OnlineStore.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public abstract class OnlineStore {
     protected Mediator mediator;
@@ -8,11 +9,75 @@ public abstract class OnlineStore {
         this.mediator=mediator;
     }
 
-    public void purchase(Product product, User user, PaymentStrategy paymentMethod, Double price) {
-        displayProduct();
+    public final void purchase() {
+
+        String name;
+        String password;
+        String email;
+        String address;
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter name: ");
+        name=scanner.nextLine();
+
+        System.out.println("Enter email: ");
+        email=scanner.nextLine();
+
+        System.out.println("Enter password: ");
+        password=scanner.nextLine();
+
+        System.out.println("Enter address: ");
+        address=scanner.nextLine();
+
+        User user = new User(name,email,password,address);
+
         Login(user);
-        processPayment(paymentMethod);
-        sendConfirmationEmail(product, user);
+        displayProduct();
+
+
+        Integer index=0;
+
+        String checkout="";
+
+        System.out.println("Add to cart: ");
+
+
+        while(true) {
+            System.out.print("Enter a number or type 'checkout' to exit: ");
+            checkout = scanner.nextLine();
+
+            if(checkout.equalsIgnoreCase("checkout")) {
+                break;
+            }
+            try {
+                int number = Integer.parseInt(checkout);
+                addProductToCart(number-1);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number or 'checkout'.");
+            }
+        }
+        System.out.println("Enter payment method: ");
+
+        System.out.println("1. Credit Card/t 2. Paypal 3. Cryptocurrency");
+
+        index=scanner.nextInt();
+        PaymentStrategy paymentStrategy=null;
+
+        if(index==1){
+            paymentStrategy=new CreditCardPayment().factoryMethod();
+        }
+        else if(index==2){
+            paymentStrategy=new PaypalPayment().factoryMethod();
+        }
+        else if(index==3){
+            paymentStrategy=new CryptoPayment().factoryMethod();
+        }
+        System.out.println("Confirming payment");
+
+        processPayment(paymentStrategy);
+
+        System.out.println("Thank you for using the system!");
     }
 
     protected abstract void displayProduct();
